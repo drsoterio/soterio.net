f6e380fb120a9a7002007391037a7a924f2f3be5
Carolina Sotério <cs4465@tc.columbia.edu>
2025-02-16 01:59:21 -0500

    test

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const ctaButton = document.querySelector(".cta-button");
-
-  // Add a simple animation to the CTA button when it's clicked
-  ctaButton.addEventListener("click", () => {
-    ctaButton.classList.add("clicked");
-    setTimeout(() => ctaButton.classList.remove("clicked"), 300);
-  });
-});
